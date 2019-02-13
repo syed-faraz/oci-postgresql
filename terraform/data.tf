cf4bf624c0ff016084dfd40289289a912fc89a8b
Piotr Matejewski <piotr.matejewski@oracle.com>
2019-02-13 15:23:04 +0100

    .

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,6 @@
 # data.tf
 data "oci_identity_availability_domains" "ads" {
-  compartment_id = "${var.tenancy_ocid}"
+  compartment_id = "${var.compartment_ocid}"
 }
 locals {
   ad1 = "${lookup(data.oci_identity_availability_domains.ads.availability_domains[0], "name")}"
@@ -8,7 +8,7 @@ locals {
   ad3 = "${lookup(data.oci_identity_availability_domains.ads.availability_domains[2], "name")}"
 }
 data "oci_core_images" "oracle_linux_image" {
-  compartment_id = "${var.tenancy_ocid}"
+  compartment_id = "${var.compartment_ocid}"
   display_name = "Oracle-Linux-7.6-2018.11.19-0"
 }
 locals  {

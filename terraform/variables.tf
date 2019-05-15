1fce76f98d64148dcfbfdbd4ddd5a21926d2f6b0
Oguz Pastirmaci <oguzp@outlook.com>
2019-05-15 15:32:46 -0700

    Oracle-Linux-7.6-2019.05.14-0 and new regions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,15 +26,17 @@ variable "password" {
 }
 
 // https://docs.cloud.oracle.com/iaas/images/image/cf34ce27-e82d-4c1a-93e6-e55103f90164/
-// Oracle-Linux-7.6-2019.03.22-1
+// Oracle-Linux-7.6-2019.05.14-0
 variable "images" {
   type = "map"
 
   default = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaydd7mt37cpouxlq2zreiiy2xauebkcwv7g3nqwziisdjenc2sz5a"
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaajnvrzitemn2k5gfkfq2hwfs4bid577u2jbrzla42wxo2qc77gwxa"
-    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaa6fy4rewjwz2kanrc47fnei5xhfsrpmei2fwfepff3drzmsexznpq"
-    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa3rebuz4fyupv2xgdsdhtoiu5aiunl7pb76hfqqevoutnxznjn5ua"
-    ca-toronto-1   = "ocid1.image.oc1.ca-toronto-1.aaaaaaaaq4fdhvuv5ylctfsjdvcfavcu233i4nkpw2zbd6bowolvi6uf3cwq"
+    ap-seoul-1     = "ocid1.image.oc1.ap-seoul-1.aaaaaaaalhbuvdg453ddyhvnbk4jsrw546zslcfyl7vl4oxfgplss3ovlm4q"
+    ap-tokyo-1     = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaamc2244t7h3gwrrci5z4ni2jsulwcg76gugupkb6epzrypawcz4hq"
+    ca-toronto-1   = "ocid1.image.oc1.ca-toronto-1.aaaaaaaakjkxzw33dcocgu2oylpllue34tjtyngwru7pcpqn4qh2nwon7n7a"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaandqh4s7a3oe3on6osdbwysgddwqwyghbx4t4ryvtcwk5xikkpvhq"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa2xe7cufdwkksdazshtmqaddgd72kdhiyoqurtoukjklktf4nxkbq"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaa4bfsnhv2cd766tiw5oraw2as7g27upxzvu7ynqwipnqfcfwqskla"
+    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaavtjpvg4njutkeu7rf7c5lay6wdbjhd4cxis774h7isqd6gktqzoa"
   }
 }
